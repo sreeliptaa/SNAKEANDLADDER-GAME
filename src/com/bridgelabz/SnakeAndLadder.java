c766f010dcd571cc3d734cfc20a381cc8a1fcd16
sreelipta <sreelipta65@gmail.com>
2021-08-08 02:07:17 +0530

    Added: roll dice to get no between 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,7 +3,9 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int startPosition = 0;                           // initialize startPosition to zero
+        int diceroll = (int)(Math.floor(Math.random() * 6) +1);
         System.out.println("WELCOME TO SNAKE AND LADDER GAME");
-        System.out.println("Single Player Game Started At Position: " + startPosition);
+        System.out.println("Diceroll You Got: " + diceroll);
+
     }
 }
