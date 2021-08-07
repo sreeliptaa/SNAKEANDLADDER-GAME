7790cdf04217d68dee5da190f4f62526aa0b0af7
sreelipta <sreelipta65@gmail.com>
2021-08-08 02:01:58 +0530

    Added: single player started the game at position zero

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,6 +2,8 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
+        int startPosition = 0;                           // initialize startPosition to zero
         System.out.println("WELCOME TO SNAKE AND LADDER GAME");
+        System.out.println("Single Player Game Started At Position: " + startPosition);
     }
 }
