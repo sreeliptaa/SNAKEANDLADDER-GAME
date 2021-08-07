41e2405b791e4b6a03cc256bf38ec7552d59f456
sreelipta <sreelipta65@gmail.com>
2021-08-08 03:59:52 +0530

    Refactor: count no of times dice rolled

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -6,47 +6,46 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
         int PLAYER_POSITION = 0;
+        int diceroll_counter = 0;
         System.out.println("WELCOME TO SNAKE AND LADDER GAME");
+
+        //constants for option
+        int noPlay = 0;
+        int ladder = 1;
+        int snake = 2;
 
 
         while (PLAYER_POSITION < WINNING_POSITION) {
             int diceroll = (int) (Math.random() * 6 + 1);   // For Generating random 1 to 6 Number
+            diceroll_counter++;
             int forWinGame = PLAYER_POSITION - diceroll;
             System.out.println("Diceroll You Got: " + diceroll);
-
-
-
-
-              //constants for option
-              int noPlay = 0;
-              int ladder = 1;
-              int snake = 2;
-
             int optionCheck = (int) (Math.random() * 3);    // To check for options
-                if (forWinGame >= diceroll)
-            // Condition checking
-            switch (optionCheck) {
-                case 0:
-                    System.out.println("No Play "); // PLAYER_POSITION IS 0
+            if (forWinGame >= diceroll)
+                // Condition checking
+                switch (optionCheck) {
+                    case 0:
+                        System.out.println("No Play "); // PLAYER_POSITION IS 0
+                        break;
+                    case 1:
+                        System.out.println("You got Ladder");
+                        PLAYER_POSITION += diceroll;
+                        break;
+                    case 2:
+                        System.out.println("You got Snake");
+                        PLAYER_POSITION -= diceroll;
 
-                    break;
-                case 1:
-                    System.out.println("You got Ladder");
-                    PLAYER_POSITION += diceroll;
-                    break;
-                case 2:
-                    System.out.println("You got Snake");
-                    PLAYER_POSITION -= diceroll;
+                        if (PLAYER_POSITION < 0) {
+                            System.out.println("You have to start from Zero ");
+                            PLAYER_POSITION = 0;
+                        }
+                        break;
+                }
 
-                    if (PLAYER_POSITION < 0) {
-                        System.out.println("You have to start from Zero ");
-                        PLAYER_POSITION = 0;
-                    }
-                    break;
-            }
-            System.out.println("Your position is :" + PLAYER_POSITION);
         }
+        System.out.println("Your position is :" + PLAYER_POSITION);
         System.out.println("You win the game ");
+        System.out.println("Dice rolled " + diceroll_counter + " times.");
 
     }
 }
